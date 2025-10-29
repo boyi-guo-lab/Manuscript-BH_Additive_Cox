b72e87660b5f71267a06b5c28e83e0af8249c7fa
Sophie Huebler <sophhuebler@gmail.com>
2025-10-28 20:50:11 -0600

    Fixing resPath

--- Sim/Code/main_R_parallelized.R
+++ Sim/Code/main_R_parallelized.R
@@ -487,7 +487,7 @@ ret <- list(
 )
 
 out_dir <- if (exists("resPath")) resPath else "Sim/Res"
-saveRDS(ret, file.path(out_dir, job_name, sprintf("it_%s.rds", it)))
+saveRDS(ret, file.path(out_dir, sprintf("it_%s.rds", it)))
 
 # Recommendation: to save the results in individual rds files
 #saveRDS(ret,
